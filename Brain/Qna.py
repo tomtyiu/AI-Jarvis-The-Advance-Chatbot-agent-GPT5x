6f6928f640326f55e55aab9c1a724512a10cbb62
Thomas YIu <tom.tyiu@gmail.com>
2026-01-13 10:00:39 -0800

    Update Qna.py

--- Brain/Qna.py
+++ Brain/Qna.py
@@ -22,19 +22,17 @@ def QuestionAnswer(question,chat_log = None):
         chat_log = chat_log_template
 
     prompt = f'{chat_log}Question : {question}\nAnswer :'
-    response = completion.create(
-        model = "text-davinci-002",
-        prompt=prompt,
-        temperature = 0,
-        max_tokens = 100,
-        top_p = 1,
-        frequency_penalty = 0,
-        presence_penalty = 0)
-    answer = response.choices[0].text.strip()
+    response = client.responses.create(
+        model = "gpt-5.2",
+        input=prompt,
+        tools=[{"type": "web_search"}],
+        )
+    answer = response.output_text.strip()
     chat_log_template_update = chat_log_template + f"\nQuestion : {question} \nAnswer : {answer}"
     Filelog = open("DataBase\\qna_log.txt","w")
     Filelog.write(chat_log_template_update)   
     Filelog.close()
     return answer     
 print(QuestionAnswer("What is the capital of India?"))    
+
 
